59fae5b97f7d51973a101ccd69c0f4a0d3f5112f
Julia Barcelos <julaai@gmail.com>
2022-03-21 14:28:33 -0300

    Created a middlewares folder. Created a talker.js file, with all demands. Called talker middleware on index.

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 
+const getTalker = require('./middlewares/talker');
+
 const app = express();
 app.use(bodyParser.json());
 
 const HTTP_OK_STATUS = 200;
 const PORT = '3000';
+
+app.get('/talker', getTalker);
 
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
