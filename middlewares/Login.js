6022b0d5da695ab291eef418ba8c8fc05e390eba
Julia Barcelos <julaai@gmail.com>
2022-03-21 16:45:56 -0300

    FIX: change to descritive name

--- middlewares/Login.js
+++ middlewares/Login.js
@@ -1,10 +1,10 @@
-const randtoken = require('rand-token');
-// https://www.npmjs.com/package/rand-token
-const token = randtoken.generate(16);
+const randToken = require('rand-token');
+// A npm to generate token: https://www.npmjs.com/package/rand-token
+const tokenNumbersAndLetters = randToken.generate(16);
 
 const makeLogin = async (req, res) => {
   try {    
-    return res.status(200).json({ token: `${token}` });
+    return res.status(200).json({ token: `${tokenNumbersAndLetters}` });
   } catch (error) {
     console.error(error);
   }
